2ba5c66f0b55c4167fa9420ab16985aa6802b7ca
sharma9178 <bsp9178@gmail.com>
2020-06-09 12:25:05 +0530

    initial

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+	document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+		var letter=this.innerHTML;
+		switch(letter){
+			case 'w': var audio=new Audio('crash.mp3');
+                      audio.play();
+			break;
+			
+			case 'a':var audio=new Audio('kick-bass.mp3');
+						audio.play();
+			break;
+			
+			case 's':var audio=new Audio('snare.mp3');
+					audio.play();
+			break;
+			
+			case 'd': var audio =new Audio('tom-1.mp3');
+					audio.play();
+			break;
+			
+			case 'j': var audio=new Audio('tom-2.mp3');
+					audio.play();
+			break;
+			
+			case 'k': var audio =new Audio('tom-3.mp3');
+					audio.play();
+			break;
+			
+			case 'l': var audio=new Audio('tom-4.mp3');
+					audio.play();
+			break;
+			
+			default: alert("wrong click");
+		}
+				
+});}
+
